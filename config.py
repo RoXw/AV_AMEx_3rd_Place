25653f833d1388a1924b13115216d40bb22a06ff
RoXw <ajaywolvex@gmail.com>
2018-11-19 16:11:02 +0530

    Final code for review
    Need to further clean the code

---
+++ config.py
@@ -0,0 +1,8 @@
+# Defining file paths
+train_path = '../input/train_amex/train.csv'
+historical_logs = '../input/train_amex/historical_user_logs.csv'
+test_path = '../input/test.csv'
+
+catboost_preds = '../utility/cat_hm_wo2.npy'
+
+submission = '../output/submission_preds.csv'
